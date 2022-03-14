5c3b63f4d7d100efb186b00bafb1d6ba3f2ff7f4
Taiwo Thomas <100496634+taiwothomas@users.noreply.github.com>
2022-03-14 02:55:56 +0000

    converted the script to a function and ran it within the script

--- LPTHW/ex33.py
+++ LPTHW/ex33.py
@@ -1,16 +1,22 @@
-i = 0
-numbers = []
+def top_bottom(bottom):
+  
+  i = 0
+  numbers = []
 
-while i < 6:
-  print(f"At the top i is {i}")
-  numbers.append(i)
+  while i < bottom:
+    print(f"At the top i is {i}")
+    numbers.append(i)
 
-  i = i + 1
-  print("Numbers now: ", numbers)
-  print(f"At the bottom i is {i}")
+    i = i + 1
+    print("Numbers now: ", numbers)
+    print(f"At the bottom i is {i}")
 
 
-print("The numbers: ")
+  print("The numbers: ")
 
-for num in numbers:
-  print(num)
+  for num in numbers:
+    print(num)
+
+print('How long is your list? ')
+bottom = int(input())
+top_bottom(bottom)
